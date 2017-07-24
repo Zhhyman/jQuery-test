ccd009b4db5134483ac6ab7cefb35c37c7a24a0b
Zhhyman <Zhyman13@gmail.com>
2017-07-24 17:23:39 -0400

    Added slider bar

--- script.js
+++ script.js
@@ -5,4 +5,8 @@ $(document).ready(function() {
     $('button').click(function() {
         $('.pink, .red').fadeTo('slow', 0);
     });
+
+    $('.pull-me').click(function() {
+        $('.panel').slideToggle('slow');
+    });
 });
