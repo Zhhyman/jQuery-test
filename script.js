421997e4e777f5883198ed9a336920bdfe6fe3be
Zhhyman <Zhyman13@gmail.com>
2017-07-24 17:19:41 -0400

    Added jQuery!

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
-$(document).ready(function() { /* Gonna run code once document is ready */
-      $('div').fadeTo(1);
+$(document).ready(function() {
+    $('button').click(function() {
+        $('.vanish').fadeOut('slow');
+    });
+    $('button').click(function() {
+        $('.pink, .red').fadeTo('slow', 0);
+    });
 });
