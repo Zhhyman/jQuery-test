ad50be6f9c49cc5d8a852b475d9e14b37aa02420
Zach <Zhyman13@gmail.com>
2017-07-25 15:00:37 -0400

    ddd

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 $(document).ready(function() {
     $('body').append("<p>I'm a paragraph!</p>");
     $('#one').after('<p>I am a p tag!</p>');    // <p></p> goes after div id one.
+<<<<<<< HEAD
     if (1+1 == 2) {
       $('#two').after('<p>cool</p>')
     };
 
+=======
+>>>>>>> ad02320b86b26d7eec237d553afe2c3317f6601f
 });
